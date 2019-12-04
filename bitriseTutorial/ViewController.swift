d3ce68278976a9fe630ea4a911e758dcac41cac2
Araib <araibkarim126@gmail.com>
2019-12-04 20:08:53 +0500

    first commit

---
+++ bitriseTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitriseTutorial
+//
+//  Created by Araib on 04/12/2019.
+//  Copyright © 2019 araibkarim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
